207b0d8d175de052c5c9c5cb22132cdfaab0d7f7
Riley Scepkowski <rscepkowski@hawkmail.hccfl.edu>
2025-04-07 21:35:03 -0400

    commit git

--- main.py
+++ main.py
@@ -3,12 +3,27 @@ This program calculates prices for custom house signs.
 """
 
 # Declare and initialize variables here.
+charge = 0.0 # charge for sign
+numChars = int(input("Enter the number of characters on the sign: ")) # type number of characters in sign 
+color = input("Enter the color of the text/characters: ").lower() # color of text/characters
+woodType = input("Enter the type of wood: ").lower() # type of wood
+
+
 # Charge for this sign.
-# Number of characters.
-# Color of characters.
-# Type of wood.
+charge = 35.00 # minimum charge
+
 
 # Write assignment and if statements here as appropriate.
+
+if numChars > 5: # if more than 5 characters, add $4 for each character over 5
+    charge += (numChars - 5) * 4 # $4 for each character over 5
+
+if woodType == "oak": # if wood type is oak, add $20
+    charge += 20.00 # add $20 for oak wood
+
+if color == "gold": # if color is gold, add $15
+    charge += 15.00 # add $15 for gold color
+
 
 # Output Charge for this sign.
 print("The charge for this sign is $" + str(charge))
